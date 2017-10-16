0450e1473e86eac38b686d7bf995797e5809f0c6
Alex Hart <alex.hart@redspace.com>
2017-10-16 11:07:12 -0300

    Update w. Correction for processing col-md-3

--- src/Main.hs
+++ src/Main.hs
@@ -8,9 +8,35 @@ import Data.List
 tesSideboardGuide :: String
 tesSideboardGuide = "http://theepicstorm.com/sideboarding-guide/"
 
-flattenCardInfo :: [String] -> [String]
-flattenCardInfo [] = []
-flattenCardInfo (x:y:xs) = (x ++ y) : flattenCardInfo xs
+data WordType s = Count s | Cardname s | None s deriving Show
+
+toWordType :: String -> WordType String
+toWordType s
+  | isPrefixOf "\n" s || isPrefixOf "-" s || isPrefixOf "+" s = Count s
+  | s == "None." = None s
+  | otherwise = Cardname s
+
+toWordTypes :: [String] -> [WordType String]
+toWordTypes = map toWordType
+
+wtToString :: WordType String -> String
+wtToString (Count s) = s
+wtToString (Cardname s) = s
+wtToString (None s) = s
+
+combineCardNames :: [WordType String] -> [WordType String]
+combineCardNames [] = []
+combineCardNames [a] = [a]
+combineCardNames (x@(None _):xs) = x : combineCardNames xs
+combineCardNames (x@(Count s):y:xs) = case y of
+  Cardname n -> combineCardNames (Count (s ++ n) : xs)
+  _ -> x : combineCardNames (y:xs)
+
+-- card header pattern is \n[+-]\d+\s
+-- Special case is "None."
+-- if we start with a card header, the next items are card names until we hit either "None." or a new header
+buildBoardInfo :: [String] -> [String]
+buildBoardInfo = map wtToString . combineCardNames . toWordTypes
 
 groupCardInfo :: [String] -> [[String]]
 groupCardInfo [] = []
@@ -26,9 +52,9 @@ formatEntry (d,c) = "<p><strong>" <> d <> "</strong> " <> formatCards c <> "</p>
 main :: IO ()
 main = do
   let doc = fromUrl tesSideboardGuide
-  decks <- runX $ doc >>> css ".clearfix .col-md-4 h5" //> getText
-  cards <- runX $ doc >>> css ".clearfix .col-md-4 p" //> getText
-  let cards' = groupCardInfo . flattenCardInfo $ cards
+  decks <- runX $ doc >>> css ".clearfix .col-md-4 h5, .clearfix .col-md-3 h5" //> getText
+  cards <- runX $ doc >>> css ".clearfix .col-md-4 p, .clearfix .col-md-3 p" //> getText
+  let cards' = groupCardInfo . buildBoardInfo $ cards
   let entries = zip decks cards'
   let entries' = map formatEntry entries
 
